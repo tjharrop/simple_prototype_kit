f92adc35e2bea90ecacb713d4b38ed55aeb37e80
Tom Harrop <tom@tomharrop.com>
2015-07-14 10:15:31 +0100

    Heroku commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include_once("index.html"); ?>
+
+<!-- This PHP file is a workaround for heroku. You cannot push an HTML, CSS and JS site to heroku.
+Adding a PHP file which effectively does nothing makes heroku treat your project as a PHP app.
+Nothing to see here, move along. -->
